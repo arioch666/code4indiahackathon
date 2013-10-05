ec9b0d19cbdd186a7c6c3def106ad65dfefc70d2
arioch666 <divij.durve@gmail.com>
2013-10-05 16:15:31 -0700

    added access to the accelerometer and proximity sensor. Done with initilizations. Need to add stream to screen

--- EmergencyAlert/src/org/arioch/emergencyalert/SensorDisplay.java
+++ EmergencyAlert/src/org/arioch/emergencyalert/SensorDisplay.java
@@ -1,23 +1,65 @@
 package org.arioch.emergencyalert;
 
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class SensorDisplay extends Activity {
+public class SensorDisplay extends Activity
+{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sensor_display);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sensor_display);
+		buildAndBind();
+		
+	}
 
+	private void buildAndBind()
+	{
+		Button start = (Button) findViewById(R.id.startButton);
+		Button stop = (Button) findViewById(R.id.stopButton);
+		initializeClickListeners(start, stop);
+		new HandleAccelerometerChange(this);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.sensor_display, menu);
-        return true;
-    }
-    
+	private void initializeClickListeners(Button start, Button stop)
+	{	
+		start.setOnClickListener(new OnClickListener()
+		{
+
+			@Override
+			public void onClick(View v)
+			{
+				findViewById(R.id.stopButton).setVisibility(v.VISIBLE);
+				findViewById(R.id.startButton).setVisibility(v.INVISIBLE);		
+				
+			}
+		});
+
+		stop.setOnClickListener(new OnClickListener()
+		{
+
+			@Override
+			public void onClick(View v)
+			{
+				findViewById(R.id.stopButton).setVisibility(v.INVISIBLE);
+				findViewById(R.id.startButton).setVisibility(v.VISIBLE);
+			}
+		});
+	}
+
+	
+
 }
