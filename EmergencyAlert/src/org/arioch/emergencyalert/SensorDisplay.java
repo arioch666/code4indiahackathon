794eb46430bc727366fe2b73b1bf3eec8e1295b8
arioch666 <divij.durve@gmail.com>
2013-10-05 17:11:08 -0700

    Deleted the second class since it was unnecessary Initialization works fine on nexus 7

--- EmergencyAlert/src/org/arioch/emergencyalert/SensorDisplay.java
+++ EmergencyAlert/src/org/arioch/emergencyalert/SensorDisplay.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
 import android.view.Menu;
@@ -13,17 +15,19 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class SensorDisplay extends Activity
+public class SensorDisplay extends Activity implements SensorEventListener
 {
-
+	Sensor accelerometer, proximitySensor;
+	SensorManager sensorManager;
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sensor_display);
 		buildAndBind();
-		
+		initilizeSensors();
 	}
 
 	private void buildAndBind()
@@ -31,19 +35,19 @@ public class SensorDisplay extends Activity
 		Button start = (Button) findViewById(R.id.startButton);
 		Button stop = (Button) findViewById(R.id.stopButton);
 		initializeClickListeners(start, stop);
-		new HandleAccelerometerChange(this);
+		
 	}
 
 	private void initializeClickListeners(Button start, Button stop)
 	{	
 		start.setOnClickListener(new OnClickListener()
 		{
-
+			
 			@Override
 			public void onClick(View v)
 			{
 				findViewById(R.id.stopButton).setVisibility(v.VISIBLE);
-				findViewById(R.id.startButton).setVisibility(v.INVISIBLE);		
+				findViewById(R.id.startButton).setVisibility(v.INVISIBLE);
 				
 			}
 		});
@@ -60,6 +64,91 @@ public class SensorDisplay extends Activity
 		});
 	}
 
-	
+	private void initilizeSensors()
+	{
+		if(initializeAccelerometer())
+		{
+			
+			if(initializeProximitySensor())
+			{
+				Toast.makeText(this, "Initilization Complete", Toast.LENGTH_SHORT).show();		
+				
+			}
+			else
+			{
+				Toast.makeText(this, "initilization failed", Toast.LENGTH_SHORT).show();
+			}
+		}
+	}
 
+	private boolean initializeProximitySensor()
+	{
+		Sensor defaultProximitySensor = ((SensorManager) 
+				getSystemService(Context.SENSOR_SERVICE)).getDefaultSensor(Sensor.TYPE_PROXIMITY);
+		
+		if (defaultProximitySensor == null)
+		{
+			Toast.makeText(this, "Your Device Does not have a proximity sensor. Buy a better Device", Toast.LENGTH_SHORT).show();
+			return false;
+		} else
+		{
+			proximitySensor = defaultProximitySensor;
+			Toast.makeText(this, "Proximity Sensor set", Toast.LENGTH_SHORT).show();
+			return true;
+		}
+	}
+
+	private Boolean initializeAccelerometer()
+	{
+		sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		
+		Sensor defaultAccelerometer = ((SensorManager) 
+				getSystemService(Context.SENSOR_SERVICE)).getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		
+		if (defaultAccelerometer == null)
+		{
+			Toast.makeText(this, "Your Device Does not have an accelerometer. Buy a better Device", Toast.LENGTH_SHORT).show();
+			return false;
+		} else
+		{
+			accelerometer = defaultAccelerometer;
+			Toast.makeText(this, "Accelerometer Set", Toast.LENGTH_SHORT).show();
+			return true;
+		}
+//		TextView stream = (TextView) context.findViewById(R.id.stream);
+//		stream.setText(displaySensorList.toString());
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor arg0, int arg1)
+	{
+
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event)
+	{
+		
+	}
+
+	@Override
+	protected void onResume()
+	{
+			super.onResume();
+			if(accelerometer!=null)
+			{
+				sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+			}
+			if(proximitySensor!=null)
+			{
+				sensorManager.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+			}			
+	}
+	
+	@Override
+	protected void onPause()
+	{
+		super.onPause();
+		sensorManager.unregisterListener(this);
+	}
 }
